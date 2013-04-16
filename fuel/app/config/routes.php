c4ac3e3d7a4bc35cec169505086cf1f38f5dc3f7
Stephan Alber <stephan.alber@theenginegroup.com>
2013-04-16 17:33:17 -0400

    routes fix

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -4,6 +4,6 @@ return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
 	
-	 'download/(:name)' => array('welcome/download', 'name' => 'download'),
+	 'download/(:name)' => array('welcome/download/$1', 'name' => 'download'),
 	// 'download/(:any)?' => 'welcome/download',
 );
